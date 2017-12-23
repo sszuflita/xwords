2aaa33247964da39d010af29b577257f64d7f6d6
Sam Szuflita <sam.szuflita@gmail.com>
2017-12-23 14:29:40 -0500

    Revert "Cache within trie too"
    This reverts commit 2dec1ceb9cf12b5a39064fb089cba4698159b4ca.

--- src/main/java/xwords/wordset/trie/TrieWordSet.java
+++ src/main/java/xwords/wordset/trie/TrieWordSet.java
@@ -1,63 +1,23 @@
 package xwords.wordset.trie;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.LoadingCache;
-import com.google.common.collect.ImmutableSet;
 import xwords.PartialFill;
 import xwords.Tile;
 import xwords.wordset.WordSet;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TrieWordSet implements WordSet {
 
 
     private final TrieNode root;
     private final Set<String> validWords;
-    private final LoadingCache<CacheKey, Set<String>> validWordSetCache;
-
-    class CacheKey {
-        private final List<Tile> tiles;
-        private final TrieNode node;
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            CacheKey cacheKey = (CacheKey) o;
-            return Objects.equals(tiles, cacheKey.tiles) &&
-                    Objects.equals(node, cacheKey.node);
-        }
-
-        @Override
-        public int hashCode() {
-
-            return Objects.hash(tiles, node);
-        }
-
-        CacheKey(List<Tile> tiles, TrieNode node) {
-            this.tiles = tiles;
-            this.node = node;
-        }
-
-        public List<Tile> getTiles() {
-            return tiles;
-        }
-
-        public TrieNode getNode() {
-            return node;
-        }
-    }
 
     public TrieWordSet(Set<String> validWords) {
         this.root = buildTrie(validWords);
         this.validWords = validWords;
-        this.validWordSetCache = Caffeine.newBuilder()
-                .recordStats()
-                .build(key -> validWordSet(key.getTiles(), key.getNode()));
     }
 
     private TrieNode buildTrie(Set<String> validWords) {
@@ -87,43 +47,42 @@ public class TrieWordSet implements WordSet {
 
     @Override
     public Set<String> validWords(PartialFill partialFill) {
-        return validWordSetCache.get(new CacheKey(partialFill.getTiles(), root));
+        return validWordsStream(partialFill.getTiles(), root).collect(Collectors.toSet());
     }
 
-
-
-    private Set<String> validWordSet(List<Tile> tiles, TrieNode node) {
+    private Stream<String> validWordsStream(List<Tile> tiles, TrieNode node) {
         if (tiles.isEmpty() && node.isWord()) {
-            return ImmutableSet.of(node.prefix());
+            return Stream.of(node.prefix());
         }
         if (tiles.isEmpty() && !node.isWord()) {
-            return ImmutableSet.of();
+            return Stream.empty();
         }
 
         if (!tiles.contains(Tile.EMPTY)) {
             String potentialString = node.prefix() + tiles.stream().map(Tile::toString).collect(Collectors.joining());
             if (validWords.contains(potentialString)) {
-                return ImmutableSet.of(potentialString);
+                return Stream.of(potentialString);
             }
-            return ImmutableSet.of();
+            return Stream.empty();
         }
         Tile tile = tiles.get(0);
         List<Tile> remainingTiles = tiles.subList(1, tiles.size());
         if (tile.equals(Tile.EMPTY)) {
             return node.children().values().stream()
-                    .flatMap(childNode -> validWordSet(remainingTiles, childNode).stream())
-                    .collect(Collectors.toSet());
+                    .flatMap(childNode -> validWordsStream(remainingTiles, childNode));
         }
         Character character = tile.name().charAt(0);
         if (!node.children().containsKey(character)) {
-            return ImmutableSet.of();
+            return Stream.of();
         }
 
-        return validWordSet(tiles.subList(1, tiles.size()), node.children().get(character));
+        return validWordsStream(tiles.subList(1, tiles.size()), node.children().get(character));
     }
 
     @Override
     public boolean isFillFeasible(PartialFill partialFill) {
-        return !validWordSetCache.get(new CacheKey(partialFill.getTiles(), root)).isEmpty();
+        return validWordsStream(partialFill.getTiles(), root).findAny().isPresent();
     }
+
+
 }
